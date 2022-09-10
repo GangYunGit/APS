eddd26f925b5accc82d1473958fcb841361c5df6
이강윤 <lky61010@naver.com>
2022-09-10 15:28:01 +0900

    Solved BOJ_2116

--- self_study/BOJ/2116_주사위_쌓기/s1.py
+++ self_study/BOJ/2116_주사위_쌓기/s1.py
@@ -3,21 +3,61 @@
 import sys
 sys.stdin = open("input.txt")
 
-N = int(input())
-side = []
-face_side = []
+num_of_dice = int(input())
+dice_list = [list(map(int, input().split())) for _ in range(num_of_dice)]
+max_of_side_sum = 0
 
-dice_list = [list(map(int, input().split())) for _ in range(N)]
+for choice in range(6):
+    # print(dice_list)
+    side_sum = 0
+    for i in range(num_of_dice):
+        sides = [1, 2, 3, 4, 5, 6]
 
-print(dice_list)
+        # 맨 아래에 있는 주사위의 윗 면(old_bottom *top값이 old_bottom에 들어감)과 바닥 면(next_bottom)을 구함
+        if i == 0:
+            next_bottom = dice_list[i][choice]
+            if dice_list[i][choice] == next_bottom:
+                if choice == 0:
+                    top = dice_list[i][5]
+                elif choice == 1:
+                    top = dice_list[i][3]
+                elif choice == 2:
+                    top = dice_list[i][4]
+                elif choice == 3:
+                    top = dice_list[i][1]
+                elif choice == 4:
+                    top = dice_list[i][2]
+                elif choice == 5:
+                    top = dice_list[i][0]
+                old_bottom = top
+        # -------------------------------------------------------------------------------------------
 
-for i in range(N):
-    for j in range(6):
-        if j == 0 or j == 1 or j == 2:
-            side.append(dice_list[i][j])
-        elif j == 5 or j == 3 or j == 4:
-            face_side.append(dice_list[i][j])
+        # 다음에 쌓인 주사위들의 윗면, 아랫면 값을 구함
+        else:
+            for choice2 in range(6):
+                if dice_list[i][choice2] == next_bottom:
+                    if choice2 == 0:
+                        top = dice_list[i][5]
+                    elif choice2 == 1:
+                        top = dice_list[i][3]
+                    elif choice2 == 2:
+                        top = dice_list[i][4]
+                    elif choice2 == 3:
+                        top = dice_list[i][1]
+                    elif choice2 == 4:
+                        top = dice_list[i][2]
+                    else:
+                        top = dice_list[i][0]
+            old_bottom = next_bottom
+            next_bottom = top
+        # -----------------------------------------------
 
-print(side)
-print(face_side)
+        sides.remove(old_bottom)
+        sides.remove(next_bottom)
+        side_sum += max(sides)
+        if side_sum >= max_of_side_sum:
+            max_of_side_sum = side_sum
+        # print(old_bottom, next_bottom)
+        # print()
 
+print(max_of_side_sum)
