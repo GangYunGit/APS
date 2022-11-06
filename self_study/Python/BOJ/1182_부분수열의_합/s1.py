0d0fc476710a0c401520d0efb7b0179dc544535a
이강윤 <lky61010@naver.com>
2022-11-06 23:42:11 +0900

    Solved BOJ_1182

--- self_study/Python/BOJ/1182_부분수열의_합/s1.py
+++ self_study/Python/BOJ/1182_부분수열의_합/s1.py
@@ -1,20 +1,18 @@
 # BOJ_1182. 부분수열의 합
 
 
-def combinations(num_list, start):
+def combinations(index, start):
     global count
 
-    if pick and sum(pick) == sum_nums:
-        print(pick)
+    if index >= nums:
+        return
+
+    start += num_list[index]
+    if start == sum_nums:
         count += 1
 
-    for i in range(start, nums):
-        if not used[i]:
-            used[i] = True
-            pick.append(num_list[i])
-            combinations(num_list, start + 1)
-            pick.pop()
-            used[i] = False
+    combinations(index + 1, start - num_list[index])
+    combinations(index + 1, start)
 
 
 nums, sum_nums = map(int, input().split())
@@ -25,6 +23,6 @@ used = [False for _ in range(nums)]
 pick = []
 result = []
 count = 0
-combinations(num_list, 0)
+combinations(index=0, start=0)
 
 print(count)
