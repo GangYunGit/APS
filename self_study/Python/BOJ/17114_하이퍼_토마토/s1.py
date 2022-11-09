85829c451caf108f6d4cc825db31626153c4f593
GangYunGit <lky61010@naver.com>
2022-11-09 17:49:59 +0900

    Unsolved BOJ_17114

---
+++ self_study/Python/BOJ/17114_하이퍼_토마토/s1.py
@@ -0,0 +1,6 @@
+# BOJ_17114. 하이퍼 토마토
+
+m, n, o, p, q, r, s, t, u, v, w = map(int, input().split())
+house = [list(map(int, input().split())) for _ in range(n * o * p * q * r * s * t * u * v)]
+
+print(house)
