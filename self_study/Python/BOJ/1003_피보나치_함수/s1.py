be4be5faa326deb0fd28d285160a6f09f2df4ba9
이강윤 <lky61010@naver.com>
2022-11-12 22:47:18 +0900

    Upload BOJ_1003

---
+++ self_study/Python/BOJ/1003_피보나치_함수/s1.py
@@ -0,0 +1,11 @@
+# BOJ_1003. 피보나치 함수
+
+dp = [0 for _ in range(42)]
+dp[0] = 1
+
+for i in range(2, 42):
+    dp[i] = dp[i - 2] + dp[i - 1]
+
+for test_case in range(1, int(input()) + 1):
+    n = int(input())
+    print(dp[n], dp[n + 1])
