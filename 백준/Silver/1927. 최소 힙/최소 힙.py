f619905dc49c35b2ac216aa375983471aafa6a75
이강윤(Gangyun Lee) <109258306+GangYunGit@users.noreply.github.com>
2022-11-15 01:45:41 +0900

    [Silver II] Title: 최소 힙, Time: 188 ms, Memory: 36764 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,15 @@
+import sys
+from heapq import heappop, heappush
+input = sys.stdin.readline
+
+n = int(input())
+heap = []
+for _ in range(n):
+    input_num = int(input())
+    if input_num != 0:
+        heappush(heap, input_num)
+    else:
+        try:
+            print(heappop(heap))
+        except:
+            print(0)
