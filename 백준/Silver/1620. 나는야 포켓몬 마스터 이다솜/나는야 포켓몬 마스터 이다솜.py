7b024f5fa5a170f48166dd96316a4e306b9d6128
이강윤(Gangyun Lee) <109258306+GangYunGit@users.noreply.github.com>
2022-11-15 02:59:32 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 260 ms, Memory: 55740 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+poketmon_dictionary = {i + 1: input().rstrip() for i in range(n)}
+reverse_dictionary = dict()
+for key, value in poketmon_dictionary.items():
+    reverse_dictionary.setdefault(value, key)
+
+for i in range(m):
+    input_data = input().rstrip()
+    if input_data.isdecimal():
+        print(poketmon_dictionary[int(input_data)])
+    else:
+        print(reverse_dictionary[input_data])
