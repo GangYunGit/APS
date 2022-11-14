1d31ed20cee2ad2f2c36478c5d26ba20b7427f45
이강윤(Gangyun Lee) <109258306+GangYunGit@users.noreply.github.com>
2022-11-15 01:26:03 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
@@ -0,0 +1,15 @@
+n = int(input())
+count_2 = 0
+count_5 = 0
+for i in range(1, n + 1):
+    while not i % 2:
+        if i % 2 == 0:
+            count_2 += 1
+            i //= 2
+
+    while not i % 5:
+        if i % 5 == 0:
+            count_5 += 1
+            i //= 5
+
+print(min(count_2, count_5))
