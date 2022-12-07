a1e148e1327bf79dc710dd7be15b03e409924908
이강윤(Gangyun Lee) <109258306+GangYunGit@users.noreply.github.com>
2022-12-07 17:10:20 +0900

    [Gold IV] Title: 알파벳, Time: 7828 ms, Memory: 150112 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+di = [-1, 0, 1, 0]
+dj = [0, 1, 0, -1]
+
+
+def dfs(i, j, count):
+    global max_count
+    max_count = max(max_count, count)
+    alphabet_list.add(board[i][j])
+
+    for direction in range(4):
+        check_i = i + di[direction]
+        check_j = j + dj[direction]
+
+        if (
+                0 <= check_i < row and 0 <= check_j < col
+                and board[check_i][check_j] not in alphabet_list
+        ):
+            dfs(check_i, check_j, count + 1)
+
+    alphabet_list.remove(board[i][j])
+    count -= 1
+
+
+row, col = map(int, input().split())
+board = [list(input()) for _ in range(row)]
+
+alphabet_list = set()
+max_count = 0
+dfs(0, 0, 1)
+
+print(max_count)
