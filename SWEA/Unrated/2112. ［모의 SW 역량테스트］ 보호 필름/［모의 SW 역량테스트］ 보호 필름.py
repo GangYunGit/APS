c405a91631bf12eb21115e9d7364ea910a95cb3e
이강윤(Gangyun Lee) <109258306+GangYunGit@users.noreply.github.com>
2022-12-11 19:51:48 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 4,220 ms, Memory: 79,736 KB -BaekjoonHub

--- SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.py
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.py
@@ -1,6 +1,6 @@
 from itertools import product
-
-
+ 
+ 
 def is_test_passed(film):
     for j in range(col):
         cell = 1
@@ -9,55 +9,57 @@ def is_test_passed(film):
                 cell = 1
             else:
                 cell += 1
-
+                
             if cell >= k:
                 break
-
+                
         if cell < k:
             return False
+        
     return True
-
-
+ 
+ 
 def inject_chemicals(film, start, end, film_row):
     global result
     picked_length = len(picked_rows)
     if picked_length == end:
         for chemicals_type in product([0, 1], repeat=picked_length):
             t = 0
-            former_rows = []
+            former_row = []
             for picked_row in picked_rows:
-                former_rows.append((picked_row, film[picked_row][:]))
-                film[picked_row] = [chemicals_type[t] for _ in range(col)]
+                former_row.append((picked_row, film[picked_row][:]))
+                for j in range(col):
+                    film[picked_row][j] = chemicals_type[t]
                 t += 1
             if is_test_passed(film):
-                for former_row in former_rows:
-                    film[former_row[0]] = former_row[1]
+                for info in former_row:
+                    film[info[0]] = info[1]
                 result = end
                 return
-
-            for former_row in former_rows:
-                film[former_row[0]] = former_row[1]
-
+ 
+            for info in former_row:
+                film[info[0]] = info[1]
+                 
         return
-
+ 
     for i in range(start, film_row):
         picked_rows.append(i)
         inject_chemicals(film, i + 1, end, film_row)
         if result > 0:
             return
         picked_rows.pop()
-
-
+ 
+ 
 for test_case in range(1, int(input()) + 1):
     row, col, k = map(int, input().split())
     film = [list(map(int, input().split())) for _ in range(row)]
     result = 0
-
+ 
     if not is_test_passed(film):
         for inject in range(1, row + 1):
             picked_rows = []
             inject_chemicals(film, 0, inject, film_row=row)
             if result > 0:
                 break
-
+ 
     print(f'#{test_case} {result}')
