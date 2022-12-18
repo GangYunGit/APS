703c5fddc4babdc96a424b651241f8e5b67f2925
이강윤(Gangyun Lee) <109258306+GangYunGit@users.noreply.github.com>
2022-12-18 20:32:03 +0900

    [Gold V] Title: 탑, Time: 652 ms, Memory: 107748 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.py
@@ -0,0 +1,16 @@
+N = int(input())
+tower_list = list(map(int, input().split()))
+
+stack = []
+result = [0] * N
+for i in range(N - 1, -1, -1):
+    if i == N - 1:
+        stack.append((i, tower_list[i]))
+    else:
+        while stack and tower_list[i] >= stack[-1][1]:
+            idx, height = stack.pop()
+            result[idx] = i + 1
+
+        stack.append((i, tower_list[i]))
+
+print(*result)
