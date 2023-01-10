a2b7a04f6a46842f68c4c1ee83d7781c764299ff
이강윤(Gangyun Lee) <109258306+GangYunGit@users.noreply.github.com>
2023-01-10 21:45:47 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 124 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
@@ -1,10 +1,10 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		String N = sc.next();
+		String N = br.readLine();
 		int[] placeNum = new int[9];
 		for(int i = 0; i < 9; i++) {
 			if(i == 0) {
