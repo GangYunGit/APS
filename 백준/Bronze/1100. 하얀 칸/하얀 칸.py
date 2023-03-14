7503ae854c8c6d09097b7a9f7fecdbb523e5cf83
이강윤 <109258306+GangYunGit@users.noreply.github.com>
2023-03-14 23:59:39 +0900

    [Bronze II] Title: 하얀 칸, Time: 84 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.py
@@ -0,0 +1,14 @@
+board = [input() for _ in range(8)]
+count = 0
+
+for i in range(8):
+    for j in range(8):
+        if i % 2:
+            if j % 2:
+                if board[i][j] == 'F':
+                    count += 1
+        else:
+            if j % 2 == 0 and board[i][j] == 'F':
+                count += 1
+
+print(count)
