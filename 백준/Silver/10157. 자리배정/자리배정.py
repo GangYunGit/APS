dc9a58504f7f21fd613d408ff32c7a719bef788f
이강윤 <109258306+GangYunGit@users.noreply.github.com>
2023-03-21 23:10:19 +0900

    [Silver IV] Title: 자리배정, Time: 752 ms, Memory: 65796 KB -BaekjoonHub

---
+++ 백준/Silver/10157. 자리배정/자리배정.py
@@ -0,0 +1,26 @@
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+C, R = map(int, input().split())
+K = int(input())
+
+grid = [[0] * R for _ in range(C)]
+
+x, y = 0, 0
+direction = 0
+for num in range(1, R * C + 1):
+    grid[x][y] = num
+    if grid[x][y] == K:
+        print(x + 1, y + 1)
+        break
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+    if 0 <= nx < C and 0 <= ny < R and grid[nx][ny] == 0:
+        x, y = nx, ny
+    else:
+        direction = (direction + 1) % 4
+        x += dx[direction]
+        y += dy[direction]
+
+if K > R * C:
+    print(0)
