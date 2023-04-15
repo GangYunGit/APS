dfe37da5faedf57c635d2008c995381400a07bfc
이강윤 <109258306+GangYunGit@users.noreply.github.com>
2023-04-15 23:59:20 +0900

    [Bronze III] Title: 최댓값, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -0,0 +1,20 @@
+board = [list(map(int, input().split())) for _ in range(9)]
+row_max = 0
+real_max = 0
+
+for i in range(9):
+    for j in range(9):
+        if board[i][j] > row_max:
+            row_max = board[i][j]
+    if row_max > real_max:
+        real_max = row_max
+
+for i in range(9):
+    for j in range(9):
+        if board[i][j] == real_max:
+            max_row_idx = i + 1
+            max_col_idx = j + 1
+            break
+
+print(real_max)
+print(max_row_idx, max_col_idx)
