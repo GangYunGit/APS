732c2798530d6e7f311afb2f93657312f1426027
이강윤 <109258306+GangYunGit@users.noreply.github.com>
2023-04-26 22:00:47 +0900

    [Silver II] Title: 아기 상어 2, Time: 4264 ms, Memory: 32524 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.py
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.py
@@ -2,44 +2,52 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-
 # 상 우상 우 우하 하 좌하 좌 좌상
 di = [-1, -1, 0, 1, 1, 1, 0, -1]
 dj = [0, 1, 1, 1, 0, -1, -1, -1]
 
 
 def bfs(i, j):
-    global max_depth
+
     global depth
-    visited[i][j] = True
+    depth -= 1
     queue = deque()
     queue.append((i, j, depth))
+    visited[i][j] = True
 
     while queue:
-        if depth > max_depth:
-            max_depth = depth
         temp_i, temp_j, depth = queue.popleft()
-        if space[temp_i][temp_j] == 1:
-            break
+
         for direction in range(8):
             next_i = temp_i + di[direction]
             next_j = temp_j + dj[direction]
             if 0 <= next_i < row and 0 <= next_j < col and not visited[next_i][next_j]:
-                visited[next_i][next_j] = True
-                queue.append((next_i, next_j, depth + 1))
+                if space[next_i][next_j] != 1:
+                    visited[next_i][next_j] = True
+                    if space[next_i][next_j] == 0:
+                        space[next_i][next_j] = depth
+                    else:
+                        if depth > space[next_i][next_j]:
+                            space[next_i][next_j] = depth
+                    queue.append((next_i, next_j, depth - 1))
 
 
 row, col = map(int, input().split())
 space = [list(map(int, input().split())) for _ in range(row)]
-max_depth = 0
+max_distance = 0
+visited = [[False] * col for _ in range(row)]
 
 for i in range(row):
     for j in range(col):
-        if space[i][j] == 0:
+        if space[i][j] == 1 and not visited[i][j]:
             depth = 0
             visited = [[False] * col for _ in range(row)]
             bfs(i, j)
-            if depth > max_depth:
-                max_depth = depth
 
-print(max_depth)
+
+for i in range(row):
+    for j in range(col):
+        if space[i][j] < max_distance:
+            max_distance = space[i][j]
+
+print(-max_distance)
