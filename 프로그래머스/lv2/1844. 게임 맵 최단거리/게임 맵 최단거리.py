b48afb68886ab829bb056345eda2d7610b8a9568
이강윤 <109258306+GangYunGit@users.noreply.github.com>
2023-05-25 10:16:50 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.97 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -5,20 +5,22 @@ dj = [0, 1, 0, -1]
 
 
 def bfs(i, j, n, m, maps):
-    visited = [[False] * m for _ in range(n)]
+    visited = [[False] * m for _ in range(n)]   # 방문한 지점을 저장하기 위한 2차원 배열 생성
     queue = deque()
-    queue.append((i, j, 1))
+    depth = 1
+    queue.append((i, j, depth))                 # 시작 지점과 bfs의 깊이(수행 횟수)를 tuple의 형태로 queue에 push
     
-    while queue:
+    while queue:                                
         pi, pj, depth = queue.popleft()
         if (pi, pj) == (n - 1, m - 1):
             return depth
         for direction in range(4):
             check_i = pi + di[direction]
             check_j = pj + dj[direction]
+            # 확인할 부분이 게임판의 범위 내에 있고, 방문하지 않았고, 벽이 아닌경우에 방문처리
             if 0 <= check_i < n and 0 <= check_j < m and not visited[check_i][check_j] and maps[check_i][check_j] != 0:
                 visited[check_i][check_j] = True
-                queue.append((check_i, check_j, depth + 1))
+                queue.append((check_i, check_j, depth + 1))     # 다음 수행을 위해 queue에 방문지점을 push
     
     return -1
 
