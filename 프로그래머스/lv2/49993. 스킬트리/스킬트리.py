ce59369f09edd91b04e6bb987d377d77526c107c
이강윤 <109258306+GangYunGit@users.noreply.github.com>
2023-06-05 20:37:54 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
@@ -1,17 +1,16 @@
 def solution(skill, skill_trees):
     answer = 0
     for skill_tree in skill_trees:
-        next_skill_index = 0
-        for custom_skill in skill_tree:
-            if custom_skill in skill:
-                if next_skill_index != skill.index(custom_skill):
+        next_skill_index = 0                # 스킬 트리의 첫 번째 스킬부터 검사하기 위해 인덱스 지정
+
+        for custom_skill in skill_tree:     # 해당 스킬트리의 스킬을 하나씩 검사
+            if custom_skill in skill:       # 스킬을 검사하면서 선행 스킬순서에 포함되어있고
+                if next_skill_index != skill.index(custom_skill):   # 검사한 스킬의 순서가 스킬트리의 순서와 맞지 않으면 break
                     break
-                else:
-                    next_skill_index += 1
+                else:                                               # 순서가 맞으면 다음 스킬을 확인하기 위해 index + 1
+                    next_skill_index += 1                           
                     
-        else:
+        else:                               # for else문 => for문을 돌면서 break문에 의해 종료되지 않았다면 else문으로 오게 됨
             answer += 1    
                     
-            
-            
     return answer
