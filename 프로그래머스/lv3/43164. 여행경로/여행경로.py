72f21bc838626c27a5b93e22b6ebb275af39ecb6
이강윤 <109258306+GangYunGit@users.noreply.github.com>
2023-06-12 15:03:27 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.py
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.py
@@ -1,23 +1,26 @@
 def solution(tickets):
-    route = ['ICN']
-    answer = []
-    tickets.sort(reverse=True)
-    print(tickets)
+    route = ['ICN']                 # 항상 ICN공항에서 시작
+    answer = []         
+    # 내림차순으로 정렬해놓으면 경로가 여러개 일 때 알파벳 순서가 가장 앞서는 공항이 마지막에 탐색됨
+    tickets.sort(key=lambda x: x[1], reverse=True)
     visited = [False for _ in range(len(tickets))]
     
+    ######## dfs 함수 시작 부분
     def dfs(next_ticket):
         nonlocal answer
-        if len(route) == len(tickets) + 1:
-            answer = route[:]
+        if len(route) == len(tickets) + 1:      # 모든 경로 탐색이 완료되었다면
+            # print(route)
+            answer = route[:]                   # answer에 경로를 저장
             return
 
         for i in range(len(tickets)):
-            if not visited[i] and tickets[i][0] == next_ticket:
-                visited[i] = True
-                route.append(tickets[i][1])
-                dfs(tickets[i][1])
-                route.pop()
-                visited[i] = False
+            if not visited[i] and tickets[i][0] == next_ticket:     # 방문하지 않았고, 다음 공항을 발견했다면
+                visited[i] = True                                   # 방문처리
+                route.append(tickets[i][1])                         # 방문한 공항을 경로에 추가
+                dfs(tickets[i][1])                                  # 다음 dfs 수행              
+                route.pop()             # return문을 만난 이후: 이미 확인한 정점을 pop
+                visited[i] = False      # 방문 처리도 해제
+    ########## dfs 끝
     
     dfs('ICN')
     
