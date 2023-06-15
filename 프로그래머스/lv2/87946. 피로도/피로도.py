54eda020681593bfebb8ea9dbc4db658dedad059
이강윤 <109258306+GangYunGit@users.noreply.github.com>
2023-06-15 22:42:25 +0900

    [level 2] Title: 피로도, Time: 0.27 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -0,0 +1,34 @@
+def solution(k, dungeons):
+    answer = -1
+    choice = []
+    visited = [False for _ in range(len(dungeons))]
+    max_count = 0
+    def dfs(end):
+        nonlocal choice, max_count
+        if len(choice) == end:
+            fatigue = k
+            count = 0
+            for i in range(len(choice)):
+                if fatigue >= choice[i][0]:
+                    count += 1
+                    fatigue -= choice[i][1]
+                else:
+                    break
+                if choice[i][1] > fatigue:
+                    break
+            
+            if count > max_count:
+                max_count = count
+            return 
+        
+        for i in range(end):
+            if not visited[i]:
+                visited[i] = True
+                choice.append(dungeons[i])
+                dfs(end)
+                visited[i] = False
+                choice.pop()
+    
+    dfs(len(dungeons))
+        
+    return max_count
