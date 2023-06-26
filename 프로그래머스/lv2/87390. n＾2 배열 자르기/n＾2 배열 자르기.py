0dbd8fb87dbb3383faa4b36c0accc3397259e0ea
이강윤 <109258306+GangYunGit@users.noreply.github.com>
2023-06-26 20:23:16 +0900

    [level 2] Title: n^2 배열 자르기, Time: 30.42 ms, Memory: 17.9 MB -BaekjoonHub

--- 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
@@ -1,5 +1,7 @@
 def solution(n, left, right):
     answer = []
     for i in range(left, right + 1):
+        # i // n => 행의 좌표, i % n => 열의 좌표
+        # 문제에서 주어진 2차원 배열의 각 칸의 값은 열과 행의 좌표 중 "크거나 같은 수 + 1"을 따라감
         answer.append(max(i // n, i % n) + 1)
     return answer
