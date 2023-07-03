a980649a9891a2442b2f00b6d506d13e6c4a52b1
이강윤 <109258306+GangYunGit@users.noreply.github.com>
2023-07-03 16:27:26 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 99.74 ms, Memory: 24.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -0,0 +1,38 @@
+from collections import defaultdict
+
+def solution(gems):
+    gem_types = set(gems)
+    gems_dict = defaultdict(int)
+    shopping_list = {}
+    results = []
+    start, end = 0, 0
+    while True:
+        if start == len(gems):
+            break 
+        
+        if len(gems_dict) == len(gem_types):
+            results.append((start, end))
+            gems_dict[gems[start]] -= 1
+            if gems_dict[gems[start]] <= 0:
+                gems_dict.pop(gems[start], None)
+            start += 1
+            continue
+        
+        if end == len(gems):
+            break
+        
+        if len(gems_dict) != len(gem_types):
+            gems_dict[gems[end]] += 1
+            end += 1
+            
+    min_gap = 100000
+    min_s, min_e = 100000, 100000
+    for s, e in results:
+        if e - s < min_gap:
+            min_gap = e - s
+            min_s = s
+            min_e = e     
+    
+    answer = [min_s + 1, min_e]
+        
+    return answer
