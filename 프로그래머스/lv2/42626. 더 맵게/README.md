976713dc26ef56ac93045c7489ca0d6757bb12ef
이강윤 <109258306+GangYunGit@users.noreply.github.com>
2023-07-11 15:31:31 +0900

    [level 2] Title: 더 맵게, Time: 1820.89 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/README.md
+++ 프로그래머스/lv2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626#) 
 
 ### 성능 요약
 
-메모리: 71.2 MB, 시간: 1779.16 ms
+메모리: 71.2 MB, 시간: 1820.89 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -3,12 +3,12 @@ from heapq import heappop, heappush
 def solution(scoville, K):
     heap = []
     count = 0
-    for food in scoville:
+    for food in scoville:       # 모든 음식을 heap에 push하면 자동으로 오름차순으로 정렬됨
         heappush(heap, food)
     while True:
-        first_food = heappop(heap)
-        if first_food >= K:
+        if heap[0] >= K:
             break
+        first_food = heappop(heap)
         second_food = heappop(heap)
         mixed_food = first_food + second_food * 2
         count += 1
