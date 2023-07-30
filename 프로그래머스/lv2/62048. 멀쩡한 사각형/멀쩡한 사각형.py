a1ed10ac3a41e5e83e044e7ee2f5feeedad0e617
이강윤 <109258306+GangYunGit@users.noreply.github.com>
2023-07-30 18:29:34 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/62048. 멀쩡한 사각형/멀쩡한 사각형.py
@@ -0,0 +1,13 @@
+import math
+
+def solution(w,h):
+    i = 1
+    a = 0
+    break_point = min(w, h)
+    while i <= break_point:
+        if not w % i and not h % i:
+            a = i
+        i += 1
+        
+    answer = w * h - (w + h - a)
+    return answer
