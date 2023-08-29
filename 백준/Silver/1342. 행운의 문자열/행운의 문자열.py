682f9206e85714955b6e5a447225df16e29e9869
이강윤 <109258306+GangYunGit@users.noreply.github.com>
2023-08-29 21:30:50 +0900

    [Silver I] Title: 행운의 문자열, Time: 3592 ms, Memory: 423868 KB -BaekjoonHub

--- 백준/Silver/1342. 행운의 문자열/행운의 문자열.py
+++ 백준/Silver/1342. 행운의 문자열/행운의 문자열.py
@@ -1,21 +1,17 @@
-def combinations():
+def combinations(prev_word):
     word_length = len(words)
 
     if len(pick) == word_length:
         new_word = "".join(pick)
-        for i in range(word_length - 1):
-            if pick[i] == pick[i + 1]:
-                return
-
         if new_word not in lucky_string:
             lucky_string.add(new_word)
         return
 
     for i in range(word_length):
-        if not used[i]:
+        if not used[i] and words[i] != prev_word:
             used[i] = True
             pick.append(words[i])
-            combinations()
+            combinations(words[i])
             pick.pop()
             used[i] = False
 
@@ -24,5 +20,5 @@ words = list(map(str, input()))
 pick = []
 used = [False for _ in range(len(words))]
 lucky_string = set()
-combinations()
+combinations("")
 print(len(lucky_string))
