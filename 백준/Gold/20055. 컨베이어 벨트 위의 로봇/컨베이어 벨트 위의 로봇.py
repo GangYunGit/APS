19167f074ffe494db6d9f6420e1d3ed9245125eb
이강윤 <109258306+GangYunGit@users.noreply.github.com>
2023-09-21 22:24:53 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 3932 ms, Memory: 34192 KB -BaekjoonHub

---
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -0,0 +1,54 @@
+from collections import deque
+
+
+def move_robot():
+    # 벨트가 각 칸 위에 있는 로봇과 함께 한 칸 회전
+    conveyor.rotate()
+
+    # 내리는 위치는 일단 내려줌
+    if conveyor[n - 1][1]:
+        conveyor[n - 1][1] = False
+
+    # 가장 먼저 벨트에 올라간 로봇부터, 벨트가 회전하는 방향으로 한 칸 이동할 수 있다면 이동
+    check_robot = []
+    for i in range(n - 2, -1, -1):
+        if conveyor[i][1]:
+            check_robot.append(i)
+
+    for i in check_robot:
+        if not conveyor[i + 1][1] and conveyor[i + 1][0] > 0:
+            conveyor[i][1] = False
+            conveyor[i + 1][1] = True
+            conveyor[i + 1][0] -= 1
+
+    # 내리는 위치는 일단 내려줌
+    if conveyor[n - 1][1]:
+        conveyor[n - 1][1] = False
+
+    # 올리는 위치 확인후 로봇 올림
+    if conveyor[0][0] != 0:
+        conveyor[0][1] = True
+        conveyor[0][0] -= 1
+
+    # 내구도 0인 칸 카운트
+    result = 0
+    for i in range(2 * n):
+        if conveyor[i][0] == 0:
+            result += 1
+
+    return result
+
+
+n, k = map(int, input().split())
+durability_info = list(map(int, input().split()))
+conveyor = deque()
+for i in range(2 * n):
+    conveyor.append([durability_info[i], False])
+
+count = 0
+while True:
+    durability_count = move_robot()
+    count += 1
+    if durability_count >= k:
+        break
+print(count)
