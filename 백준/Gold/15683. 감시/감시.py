29283c0ad90225dc0c7bc527a26c29684443231f
이강윤 <109258306+GangYunGit@users.noreply.github.com>
2023-10-12 17:28:35 +0900

    [Gold IV] Title: 감시, Time: 456 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.py
+++ 백준/Gold/15683. 감시/감시.py
@@ -1,49 +1,61 @@
-from itertools import product
-from copy import deepcopy
+# 상, 우, 하, 좌
+di = [-1, 0, 1, 0]
+dj = [0, 1, 0, -1]
 
-# 상 우 하 좌
-di = [-1, 0, 1, 0, -1, 0, 1, 0]
-dj = [0, 1, 0, -1, 0, 1, 0, -1]
 
-cctv = {1: [1], 2: [3, 1], 3: [0, 1], 4: [3, 0, 1], 5: [0, 1, 2, 3]}
+# 1번 : 우 / 2번 : 좌, 우 / 3번 : 상, 우 / 4번 : 좌, 상, 우 / 5번 : 전부
+cctv_type = {
+    1: [[0], [1], [2], [3]],
+    2: [[1, 3], [0, 2]],
+    3: [[0, 1], [1, 2], [2, 3], [3, 0]],
+    4: [[0, 1, 3], [1, 2, 0], [2, 3, 1], [3, 0, 2]],
+    5: [[0, 1, 2, 3]]
+}
 
-row, col = map(int, input().split())
-office = [list(map(int, input().split())) for _ in range(row)]
-cctv_points = []
-min_count = row * col
 
-for i in range(row):
-    for j in range(col):
-        if 0 < office[i][j] < 6:
-            cctv_points.append((i, j))
+def dfs(cctv_location, cctv_idx):
+    global min_count
+    if cctv_idx == len(cctv_location):
+        count = 0
+        for i in range(n):
+            for j in range(m):
+                if room[i][j] == 0:
+                    count += 1
+        min_count = min(min_count, count)
+        return
 
-cctv_nums = len(cctv_points)
-
-for comb in product([0, 1, 2, 3], repeat=cctv_nums):
-    new_office = deepcopy(office)
-    count = 0
-    for cctv_num in range(cctv_nums):
-        cctv_i = cctv_points[cctv_num][0]
-        cctv_j = cctv_points[cctv_num][1]
-        for direction in cctv[new_office[cctv_i][cctv_j]]:
+    cctv_i, cctv_j = cctv_location[cctv_idx]
+    for direction_list in cctv_type[room[cctv_i][cctv_j]]:
+        checked_area = []
+        for direction in direction_list:
             k = 1
-            check_i = cctv_i + k * di[direction + comb[cctv_num]]
-            check_j = cctv_j + k * dj[direction + comb[cctv_num]]
-            while 0 <= check_i < row and 0 <= check_j < col:
-                if new_office[check_i][check_j] == 0 or new_office[check_i][check_j] == 7:
-                    new_office[check_i][check_j] = 7
-                elif new_office[check_i][check_j] == 6:
+            while True:
+                check_i, check_j = cctv_i + k * di[direction], cctv_j + k * dj[direction]
+                if not (0 <= check_i < n and 0 <= check_j < m):
                     break
-                k += 1
-                check_i = cctv_i + k * di[direction + comb[cctv_num]]
-                check_j = cctv_j + k * dj[direction + comb[cctv_num]]
+                else:
+                    if room[check_i][check_j] == 6:
+                        break
+                    elif room[check_i][check_j] == 0:
+                        room[check_i][check_j] = -1
+                        checked_area.append((check_i, check_j))
+                    k += 1
 
-    for i in range(row):
-        for j in range(col):
-            if new_office[i][j] == 0:
-                count += 1
+        dfs(cctv_location, cctv_idx + 1)
+        for row, col in checked_area:
+            room[row][col] = 0
 
-    if count < min_count:
-        min_count = count
 
+n, m = map(int, input().split())
+room = [list(map(int, input().split())) for _ in range(n)]
+cctv_location = []
+
+for row in range(n):
+    for col in range(m):
+        if 0 < room[row][col] < 6:
+            cctv_location.append((row, col))
+
+min_count = n * m
+cctv_rotation = []
+dfs(cctv_location, 0)
 print(min_count)
