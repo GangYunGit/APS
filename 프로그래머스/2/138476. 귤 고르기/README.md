eae6339a10271cc7ea335577111b40879c6f0cc1
이강윤 <109258306+GangYunGit@users.noreply.github.com>
2023-11-03 20:59:13 +0900

    [level 2] Title: 귤 고르기, Time: 14.83 ms, Memory: 18.1 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/README.md
+++ 프로그래머스/2/138476. 귤 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 귤 고르기 - 138476 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476) 
 
 ### 성능 요약
 
-메모리: 18.6 MB, 시간: 64.73 ms
+메모리: 18.1 MB, 시간: 14.83 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 5일 20:49:48
+2023년 11월 5일 20:59:11
 
 ### 문제 설명
 
--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,31 +1,14 @@
+from collections import Counter
+
 def solution(k, tangerine):
     answer = 0
-    tangerine.sort()
-    tangerine_size = []
-    prev = tangerine[0]
-    count = 1
-    if len(tangerine) == 1:
-        tangerine_size.append((1, prev))
-    
-    for i in range(1, len(tangerine)):
-        if prev == tangerine[i]:
-            count += 1
-        else:
-            tangerine_size.append((count, prev))
-            count = 1
-            prev = tangerine[i]
-            
-        if i == len(tangerine) - 1:
-            tangerine_size.append((count, tangerine[i]))
-    
-    tangerine_size.sort(reverse=True)
-
+    tangerine_counter = Counter(tangerine)
     box_size = 0
-    for num, tangerine_idx in tangerine_size:
+    for size in sorted(tangerine_counter.values(), reverse=True):
         if box_size < k:
-            box_size += num
+            box_size += size
             answer += 1
         else:
             break
-            
+    
     return answer
