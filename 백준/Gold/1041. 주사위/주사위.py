ef516d2b81b713f143a65937e3e670ef97d99a6f
GangYun Lee <109258306+GangYunGit@users.noreply.github.com>
2024-04-01 20:53:04 +0900

    [Gold V] Title: 주사위, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1041. 주사위/주사위.py
+++ 백준/Gold/1041. 주사위/주사위.py
@@ -3,7 +3,6 @@ a, b, c, d, e, f = map(int, input().split())
 sum_1 = min(a, b, c, d, e, f)
 sum_2 = min(a + c, a + e, a + d, a + b, d + e, e + c, b + c, b + d, b + f, e + f, d + f, c + f)
 sum_3 = min(a + c + e, a + d + e, a + b + d, a + b + c, b + d + f, b + c + f, d + e + f, c + e + f)
-
 if n == 1:
     print(sum([a, b, c, d, e, f]) - max([a, b, c, d, e, f]))
 else:
